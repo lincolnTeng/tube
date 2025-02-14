8ed9b3765b217a4f04b10075fb92555368d467d4
lincolnTeng <timeasia@gmail.com>
2025-02-14 18:09:34 +0800

    Update item.js

--- functions/item.js
+++ functions/item.js
@@ -1,1 +1,10 @@
-
+export async function onRequest(context) {
+    const { searchParams } = new URL(context.request.url);
+    const videoId = searchParams.get('p');
+    
+    const content = `        path to ${videoId}     `;
+    
+    return new Response(content, {
+        headers: { 'Content-Type': 'text/html;charset=UTF-8' },
+    });
+}
