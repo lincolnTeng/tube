059c6abfb8396eeb0c4f8a110a5b2c23fa034765
lincolnTeng <timeasia@gmail.com>
2025-02-15 02:47:52 +0800

    Update [[dayid]].js

--- functions/item/[[dayid]].js
+++ functions/item/[[dayid]].js
@@ -1,7 +1,29 @@
 export async function onRequest(context) {
+   const{timedir,vid} = context.params.dayid ;
+   re = "got id ["+ timedir+ "] and vid :[" + vid +"]" ;
+   const fetchre  = await fetch ( "/space/"+timedir+"/"+vid +".pjson";
+   const pjson = await fetchre.json();
+   const { inf: { title, channel,upload_date,duration,view_count }, tracks }  = pjson ;
+
+    re += "<div class= 'video-profile'> " ; 
+    re += " <div class= 'video-thumb'> <span >thunb </span></div> " ; 
+
+    re += " <div class= 'video-info'>" ; 
+    re += f" <h6 class= 'video-title'> ${inf['title']}  </h6>" ; 
+    re += " <div class='video-details' > " ; 
+    re += f" <span class='vviews'> <i class='bi bi-broadcast'> </i> {$inf['channel']}</span>" ;
+    re += f" <span class='vviews'> <i class='bi bi-eye'> </i> {$inf['view_count']}</span>"; 
+    re += f" <span class='vviews'> <i class='bi bi-clock'> </i> {$inf['duration']}</span>"; 
+    re += f" <span class='vviews'> <i class='bi bi-calendar'> </i> {$inf['upload_date']}</span>" ; 
+
+    re += "</div></div></div>" ; 
+
+   
+    
+
    
     return new Response(
-        JSON.stringify(context.params.dayid), {
+        re , {
           headers: { 'Content-Type': 'text/html;charset=UTF-8' },
         }
     );
