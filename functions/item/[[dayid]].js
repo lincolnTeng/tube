eb59dd8bf21cdf9dea6c5bb17f6998187a2227f6
lincolnTeng <timeasia@gmail.com>
2025-02-15 04:01:17 +0800

    Update [[dayid]].js

--- functions/item/[[dayid]].js
+++ functions/item/[[dayid]].js
@@ -6,13 +6,13 @@ export async function onRequest(context) {
    let fetchpath = `/space/${timedir}/${vid}.pjson`;
    const fetchre  = await fetch ( fetchpath );
     const pjson = await fetchre.json();
-   const { inf: { title, channel,upload_date,duration,view_count }, tracks }  = pjson ;
-
+   const { info , tracks }  = pjson ;
+   const { title, channel,upload_date,duration,view_count } = info ;
     re += `<div class= 'video-profile'> ` ; 
     re += ` <div class= 'video-thumb'> <span >thunb </span></div> ` ; 
 
     re += ` <div class= 'video-info'>` ; 
-    re += ` <h6 class= 'video-title'> ${inf['title']}  </h6>` ; /*
+    re += ` <h6 class= 'video-title'> ${title}  </h6>` ; /*
     re += " <div class='video-details' > " ; 
     re += " <span class='vviews'> <i class='bi bi-broadcast'> </i> {$inf['channel']}</span>" ;
     re += " <span class='vviews'> <i class='bi bi-eye'> </i> {$inf['view_count']}</span>"; 
