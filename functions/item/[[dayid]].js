40ca8976a39d662b8da0de27017806f8cd7ea2bb
lincolnTeng <timeasia@gmail.com>
2025-02-15 03:54:09 +0800

    Update [[dayid]].js

--- functions/item/[[dayid]].js
+++ functions/item/[[dayid]].js
@@ -1,18 +1,18 @@
 export async function onRequest(context) {
 
    const [timedir, vid]   = context.params.dayid ;
-   //  re = "this is a string " ;
+   //  re = "this is a string " ; ` `;
    let re  = ` path is  ${timedir} to ${vid} .....  `;
-   /*
-   const fetchre  = await fetch ( "/space/${timedir}/${vid}.pjson" );
+   let fetchpath = `/space/${timedir}/${vid}.pjson`;
+   const fetchre  = await fetch ( fetchpath );
     const pjson = await fetchre.json();
    const { inf: { title, channel,upload_date,duration,view_count }, tracks }  = pjson ;
 
-    re += "<div class= 'video-profile'> " ; 
-    re += " <div class= 'video-thumb'> <span >thunb </span></div> " ; 
+    re += `<div class= 'video-profile'> ` ; 
+    re += ` <div class= 'video-thumb'> <span >thunb </span></div> ` ; 
 
-    re += " <div class= 'video-info'>" ; 
-    re += " <h6 class= 'video-title'> ${inf['title']}  </h6>" ; 
+    re += ` <div class= 'video-info'>` ; 
+    re += ` <h6 class= 'video-title'> ${inf['title']}  </h6>` ; /*
     re += " <div class='video-details' > " ; 
     re += " <span class='vviews'> <i class='bi bi-broadcast'> </i> {$inf['channel']}</span>" ;
     re += " <span class='vviews'> <i class='bi bi-eye'> </i> {$inf['view_count']}</span>"; 
