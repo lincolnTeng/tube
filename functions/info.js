8a097081af8758ca18bf65955a01e8c1eda77e48
lincolnTeng <timeasia@gmail.com>
2025-02-23 02:05:00 +0800

    Update info.js

--- functions/info.js
+++ functions/info.js
@@ -25,8 +25,8 @@ export async function onRequest(context) {
 
         }
        pcode = ws + '@' + vid + '@if' ;
-       let re = await  fetch('http://148.135.115.48/pool/prof/'+pcode ) ; 
-          
+       let fetchre = await  fetch('http://148.135.115.48/pool/prof/'+pcode ) ; 
+       let re = await fetchre.text();
        return new Response(
          re , {  headers: { 'Content-Type': 'text/html;charset=UTF-8' },    }
        );
