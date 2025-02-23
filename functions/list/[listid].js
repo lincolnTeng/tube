a963fc3608f75a5d006ab2b7189b1ce1d816a2fa
lincolnTeng <timeasia@gmail.com>
2025-02-24 03:55:28 +0800

    Update [listid].js

--- functions/list/[listid].js
+++ functions/list/[listid].js
@@ -1,7 +1,9 @@
 export async function onRequest(context) {
        const listid   = context.params.listid ;
+       let listjson ={} ;  
+   try{
        const fetchre = await fetch( `https://tube.bayx.uk/data/${listid}.list`) ; 
-       let listjson ={} ; 
+
     
        if( fetchre.ok ) 
            listjson = await fetchre.json() ;
@@ -21,5 +23,13 @@ export async function onRequest(context) {
       return new Response( re ,  {
            headers: { 'Content-Type': 'text/html;charset=UTF-8' },
       });
+
+ } catch (error) {
+    //  console.error('Error parsing FormData:', error);
+      return new Response(JSON.stringify({ error: 'Failed to parse form data ', 'listid':listid, 'listjson': listjson  }), {
+        status: 400,
+        headers: { 'Content-Type': 'application/json' },
+      });
+ }
      
 }
