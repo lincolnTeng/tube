574838bf25a298e0e492bda6c241fafc4ddb6c5a
lincolnTeng <timeasia@gmail.com>
2025-02-24 03:16:13 +0800

    Update [listid].js

--- functions/list/[listid].js
+++ functions/list/[listid].js
@@ -1,17 +1,25 @@
 export async function onRequest(context) {
-    const { searchParams } = new URL(context.request.url);
-    const videoId = searchParams.get('p');
+       const listid   = context.params.listid ;
+       const fetchre = await fetch( `https://tube.bayx.uk/data/${listid}.list`) ; 
+       const listjson ; 
     
-    const content = `
-        <div class="video-container">
-            <iframe 
-                src="https://www.youtube.com/embed/${videoId}"
-                allowfullscreen>
-            </iframe>
-        </div>
-    `;
+       if( fetchre.ok ) 
+           listjson = fetchre.json() ;
+       else 
+           return new Response( `bad list fetch ${listid}`, {
+                    headers: { 'Content-Type': 'text/html;charset=UTF-8' },
+            });
+      let re = `<div>   <div> list title : ${listjson.title} </div>` ;
+
+      for (const item of listjson.items) {
+       
+          re + = `<div id="${item.id}" > <script > re = await fetch("/item/${item.id}"); pr= await re.json();  print( pr );</script> </div> ` ; 
+      
+      } ; 
     
-    return new Response(content, {
-        headers: { 'Content-Type': 'text/html;charset=UTF-8' },
-    });
+      re += "</div>" ; 
+      return new Response( re ,  {
+           headers: { 'Content-Type': 'text/html;charset=UTF-8' },
+      });
+     
 }
