0ecff9cd62c1aa12aba31c8b901836ec6aca9cee
lincolnTeng <timeasia@gmail.com>
2025-02-25 03:34:13 +0800

    Update [[dayid]].js

--- functions/cafi/[[dayid]].js
+++ functions/cafi/[[dayid]].js
@@ -8,7 +8,7 @@ export async function onRequest(context) {
      re += ` <h6 class="panel-title bg-secondary"> ${timedir}||${vid} </h6> </div>`; 
 
    
-   let fetchpath = `https://tube.bayx.uk/space/${timedir}/${vid}.cjf`;
+   let fetchpath = `https://tube.bayx.uk/space/${timedir}/${vid}.cfj`;
    const fetchre  = await fetch ( fetchpath );
     const cafiline = await fetchre.text();
    const [ track , fid ]  = cafiline.split('=') ;
@@ -17,7 +17,7 @@ export async function onRequest(context) {
     re += ` <div class="col-md-2">`;
          
     re += `<a href="/downcafi/${fid}" download="${fid}"> `;
-    re += `<span  class="baget bg-info">${track}</span></a>`;
+    re += `<span  class="badge bg-info">${track}</span></a>`;
     re += `</div>` ;    
     return new Response(
          re , {
