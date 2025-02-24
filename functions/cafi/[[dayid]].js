5089e45121f306fce20e69e876ca48ea2c4f21d0
lincolnTeng <timeasia@gmail.com>
2025-02-25 03:11:35 +0800

    Create [[dayid]].js

---
+++ functions/cafi/[[dayid]].js
@@ -0,0 +1,30 @@
+export async function onRequest(context) {
+
+   const [timedir, vid]   = context.params.dayid ;
+   //  re = "this is a string " ; ` `;
+   let re  = ` `;
+
+     re += ` <div class="panel-heading  "> `;
+     re += ` <h6 class="panel-title bg-secondary"> ${timedir}||${vid} </h6> </div>`; 
+
+   
+   let fetchpath = `https://tube.bayx.uk/space/${timedir}/${vid}.cjf`;
+   const fetchre  = await fetch ( fetchpath );
+    const cafiline = await fetchre.text();
+   const [ track , fid ]  = cafiline.split('=') ;
+ 
+
+    re += ` <div class="col-md-2">`;
+         
+    re += `<a href="/downcafi/${fid}" download="${fid}"> `;
+    re += `<span  class="baget bg-info">${track}</span></a>`;
+    re += `</div>` ;    
+    return new Response(
+         re , {
+          headers: { 'Content-Type': 'text/html;charset=UTF-8' },
+        }
+    );
+
+
+  
+}
