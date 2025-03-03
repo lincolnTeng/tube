468d184fb882c2abc425c1acbff14998bb3c5b77
timeasia@gmail.com <timeasia@gmail.com>
2025-03-03 15:41:01 +0000

    tsettest

--- space/gitpush.sh
+++ space/gitpush.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 
-# 获取提交信息
 COMMIT_MESSAGE="$1"
 
-# 检查是否提供了提交信息
 if [ -z "$COMMIT_MESSAGE" ]; then
   echo "Usage: $0 <commit_message>"
   exit 1
 fi
 
-# 添加所有更改的文件
 git add .
 
-# 提交更改
 git commit -m "$COMMIT_MESSAGE"
 
-# 推送到 GitHub
-git push origin main
+git push daytube HEAD:main
 
 echo "Space directory updated and pushed to GitHub."
+
+
+
